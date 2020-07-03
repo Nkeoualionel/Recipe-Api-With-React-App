7f2f2064e72c79d15c643438542ee5ff8b74d40f
Lionel <lnkeoua@ludexgames.com>
2020-07-03 05:56:26 +0000

    added style from Recipe.module.css

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import style from "./Recipe.module.css";
 
-const Recipe = ({title, calories, image}) =>{
+const Recipe = ({title, calories, image, source, ingredients}) =>{
 
     return(
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
             <p>{calories}</p>
-            <img src={image} alt=""/>
+            <img className={style.image} src={image} alt=""/>
+            <p>{source}</p>
+           
         </div>
 
     );
