f62f8e03954635d2ae55338b723e0895ba419eed
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-08-13 18:55:31 +0000

    Updates

---
+++ precache-manifest.78f09fa5eea36b7d8c828f85f5720183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf19edf86bead8045447af2f01b4dac4",
+    "url": "/Recipe-Api-With-React-App/index.html"
+  },
+  {
+    "revision": "f9776e36fed75c25d13b",
+    "url": "/Recipe-Api-With-React-App/static/css/main.09d69312.chunk.css"
+  },
+  {
+    "revision": "76af69c7234fa6e537c3",
+    "url": "/Recipe-Api-With-React-App/static/js/2.4c546f0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Api-With-React-App/static/js/2.4c546f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9776e36fed75c25d13b",
+    "url": "/Recipe-Api-With-React-App/static/js/main.0067b4da.chunk.js"
+  },
+  {
+    "revision": "6e601c523fcd17e6db25",
+    "url": "/Recipe-Api-With-React-App/static/js/runtime-main.0a8cedc1.js"
+  }
+]);
